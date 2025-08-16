88fe6c0bae2fac7176cee863971ae533fd0d2bbf
ғᴀʟʑʏ <83463829+spettacolo@users.noreply.github.com>
2025-08-16 13:26:33 +0200

    Update

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,3 @@
 export default function Head() {
-  return (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-    </>
-  );
+  return null;
 }
