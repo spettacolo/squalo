99e3413ed9ebf299e328fefb1614989a96e52125
ғᴀʟʑʏ <83463829+spettacolo@users.noreply.github.com>
2025-08-16 13:19:15 +0200

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
